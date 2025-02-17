78b91cb11fdb03d7a4bb9169fa5ae29da74df19f
AmrAlsyed <Xaxeth@gmail.com>
2025-02-17 20:33:33 +0200

    fixed file path2

--- crud-frontend/src/App.jsx
+++ crud-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Modalform from "./components/Modalform.jsx";
 import NavBar from "./components/NavBar.jsx";
-import TableList from "./components/Tablelist.jsx";
+import TableList from "./components/TableList.jsx";
 import axios from "axios";
 
 function App() {
